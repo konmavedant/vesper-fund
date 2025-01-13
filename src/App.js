d22b7980209dcb0ee9d15a38648be1753b7b4e30
konmavedant <vedant@konma.io>
2025-01-13 20:49:12 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Import the CSS file
+
+function App() {
+  return (
+    <div className="fullscreen-image">
+      <img src="vesper.png" alt="Vesper Logo" className="centered-logo" />
+      <div className="coming-soon-text">Coming Soon</div>
+    </div>
+  );
+}
+
+export default App;
